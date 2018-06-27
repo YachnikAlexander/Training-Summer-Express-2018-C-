90e1f3b73a7b7603b27323ef7f02296cc3eb8436
Yachnik Alexander <sansanich230899@gmail.com>
2018-06-27 17:47:18 +0300

    added tasks from Day1

---
+++ NET.S.2018.YACHNIK.01/ArraySorting/ArraySortingTest/ArraySortTesting.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArraySortingTest
+{
+    /// <summary>
+    /// Testing class Sort, exactly Merge and Quick
+    /// </summary>
+    [TestClass]
+    public class ArraySortingTest
+    {
+        /// <summary>
+        /// Testing MergeSort for overloaded method with 1 argument 
+        /// </summary>
+        [TestMethod]
+        public void MergeSortOverloadedTest()
+        {
+            ////Aranged Act Assert (AAA) Pattern
+
+            ////Arrange
+            int[] array = new int[] { 1, 2, 77, 7, 701 };
+            int[] expected = new int[] { 1, 2, 7, 77, 701 };
+            ////Act
+            int[] actual = ArraySorting.Sort.MergeSort(array);
+            ////Asset
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// TTesting MergeSort for  method with 3 argument 
+        /// </summary>
+        [TestMethod]
+        public void MergeSortTest()
+        {
+            ////Aranged Act Assert (AAA) Pattern
+
+            ////Arrange
+            int[] array = new int[] { 1, 77, 2, 7, 701 };
+            int[] expected = new int[] { 1, 2, 77, 7, 701 };
+            ////Act
+            int[] actual = ArraySorting.Sort.MergeSort(array, 0, 2);
+            ////Asset
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Testing ArgumentException for empty array MergeSort
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void EmptyArrayMergeTest()
+        {
+            int[] array = new int[] { };
+            int[] actual = ArraySorting.Sort.MergeSort(array);
+        }
+
+        /// <summary>
+        /// Testing ArgumentNullException for null array MergeSort
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullArrayMergeTest()
+        {
+            int[] array = null;
+            int[] actual = ArraySorting.Sort.MergeSort(array);
+        }
+
+        /// <summary>
+        /// Testing MergeSort for overloaded method with 1 argument 
+        /// </summary>
+        [TestMethod]
+        public void QuickSortOverloadedTest()
+        {
+            ////Aranged Act Assert (AAA) Pattern
+            ////Arrange
+            int[] array = new int[] { 1, 2, 77, 7, 701 };
+            int[] expected = new int[] { 1, 2, 7, 77, 701 };
+            ////Act
+            int[] actual = ArraySorting.Sort.QuickSort(array);
+            ////Asset
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Testing MergeSort for method with 3 argument 
+        /// </summary>
+        [TestMethod]
+        public void QuickSortTest()
+        {
+            ////Aranged Act Assert (AAA) Pattern
+
+            ////Arrange
+            int[] array = new int[] { 1, 77, 2, 7, 701 };
+            int[] expected = new int[] { 1, 2, 77, 7, 701 };
+            ////Act
+            int[] actual = ArraySorting.Sort.QuickSort(array, 0, 2);
+            ////Asset
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Testing ArgumentException for emptyArray Quick Sort
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void EmptyArrayQuickTest()
+        {
+            int[] array = new int[] { };
+            int[] actual = ArraySorting.Sort.QuickSort(array);
+        }
+
+        /// <summary>
+        /// Testing ArgumentNullException for emptyArray Quick Sort
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullArrayQuickTest()
+        {
+            int[] array = null;
+            int[] actual = ArraySorting.Sort.QuickSort(array);
+        }
+    }
+}
