04771cb702cd057a56b0b24c2b08583a59babec6
Yachnik Alexander <sansanich230899@gmail.com>
2018-06-28 01:29:00 +0300

    Added task6 ArrayExtension

---
+++ NET.S.2018.YACHNIK.02/ArrayExtensions/ArrayExtensions/ArrayExtensions.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// extension for array to determine the digit in number
+/// </summary>
+namespace ArrayExtensions
+{
+    /// <summary>
+    /// class to determine digit
+    /// </summary>
+    public class ArrayExtensions
+    {
+        /// <summary>
+        /// extension method for validate
+        /// </summary>
+        /// <param name="source">array of numbers</param>
+        /// <param name="digit">digit what check</param>
+        /// <returns>list of number, where exist the digit</returns>
+        public static List<int> FilterNumbers(int[] source, int digit)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (source.Length == 0)
+            {
+                throw new ArgumentException(nameof(source));
+            }
+
+            if (digit < 0 || digit > 9)
+            {
+                throw new ArgumentOutOfRangeException(nameof(digit));
+            }
+
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < source.Length; i++)
+            {
+                if (IsDigit(source[i], digit))
+                {
+                    list.Add(source[i]);
+                }
+            }
+
+            return list;
+        }
+
+        /// <summary>
+        /// method to determine the existance of digit
+        /// </summary>
+        /// <param name="number">number for checking</param>
+        /// <param name="digit">digit what check</param>
+        /// <returns>return true or false based on exist of digit in number</returns>
+        private static bool IsDigit(int number, int digit)
+        {
+            if (digit != 0)
+            {
+                if (number < 0)
+                {
+                    number *= -1;
+                }
+
+                while (number != 0)
+                {
+                    int t = number % 10;
+
+                    if (t == digit)
+                    {
+                        return true;
+                    }
+
+                    number /= 10;
+                }
+            }
+            else
+            {
+                if (number == 0 || number % 10 == 0)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ NET.S.2018.YACHNIK.02/ArrayExtensions/ArrayExtensionsTest/ArrayExtensionsTest.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ArrayExtensions;
+
+/// <summary>
+/// namespace for testing ArrayExtension
+/// </summary>
+namespace ArrayExtensionsTest
+{
+    /// <summary>
+    /// class for testing methods
+    /// </summary>
+    [TestClass]
+    public class ArrayExtensionsTest
+    {
+        /// <summary>
+        /// Testing valid code for Positiv numbers
+        /// </summary>
+        [TestMethod]
+        public void PositivNumbersArrayTest()
+        {
+            int[] array = new int[] { 1, 2, 77, 7, 701 };
+            int digit = 7;
+
+            List<int> expected = new List<int>() { 77, 7, 701 };
+
+            List<int> actual = ArrayExtensions.ArrayExtensions.FilterNumbers(array, digit);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Testing valid code for Positiv and Negative numbers
+        /// </summary>
+        [TestMethod]
+        public void PositivNegativNumbersArrayTest()
+        {
+            int[] array = new int[] { 1, 2, 3, 4, 5, 6, -7, 68, 69, 70, 15, -17 };
+            int digit = 7;
+            List<int> expected = new List<int>() { -7, 70, -17 };
+            List<int> actual = ArrayExtensions.ArrayExtensions.FilterNumbers(array, digit);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// check exception for negative digit
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ArrayTest_digit()
+        {
+            int[] array = new int[] { 1, 2, 3, 4, 5, 6, -7, 68, 69, 70, 15, -17 };
+            int digit = -7;
+            List<int> actual = ArrayExtensions.ArrayExtensions.FilterNumbers(array, digit);
+        }
+
+        /// <summary>
+        /// check exception for empty Array
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ArrayEmptyTest()
+        {
+            int[] array = new int[] { };
+            int digit = 7;
+            List<int> actual = ArrayExtensions.ArrayExtensions.FilterNumbers(array, digit);
+        }
+
+        /// <summary>
+        /// check exception for null array
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ArrayNullTest()
+        {
+            int[] array = null;
+            int digit = 7;
+            List<int> actual = ArrayExtensions.ArrayExtensions.FilterNumbers(array, digit);
+        }
+
+        /// <summary>
+        /// check valid code for digit = 0
+        /// </summary>
+        [TestMethod]
+        public void ZeroDigitArrayExpensionsTest()
+        {
+            int[] array = new int[] { 0, 10, 20, 111, 456 };
+            int digit = 0;
+            List<int> expected = new List<int>() { 0, 10, 20 };
+            List<int> actual = ArrayExtensions.ArrayExtensions.FilterNumbers(array, digit);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
