a63664b3d4f45a116feccd85be30d242247502a1
Yachnik Alexander <sansanich230899@gmail.com>
2018-07-08 14:13:36 +0300

    Added Polynomial day5

---
+++ NET.S.2018.YACHNIK.05-06/Polynomial/Polinomial.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolinomialExtension
+{
+    public class Polinomial
+    {
+        private readonly double[] coefs;
+        private readonly int degree;
+
+        public Polinomial(double[] coefs, int degree)
+        {
+            IsValid(coefs, degree);
+            this.coefs = coefs;
+            this.degree = degree;
+        }
+
+        public Polinomial(double[] coefs)
+        {
+            IsValid(coefs, degree);
+            int length = coefs.Length;
+            this.coefs = coefs;
+            this.degree = length - 1;
+        }
+
+        public Polinomial(int degree)
+        {
+            if (degree < 0)
+            {
+                throw new ArgumentException(nameof(degree));
+            }
+
+            List<double> arr = new List<double>();
+
+            for(int i = 0; i < degree + 1; i++)
+            {
+                arr.Add(1);
+            }
+
+            this.degree = degree;
+            this.coefs = arr.ToArray();
+        }
+
+        public Polinomial()
+        {
+            this.coefs = new double[]{1, 1, 1};
+            this.degree = 2;
+        }
+
+        public double[] getCoef()
+        {
+            return coefs;
+        }
+
+        public int getDegree()
+        {
+            return degree;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+
+            Polinomial polin = obj as Polinomial;
+
+            return this == polin;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 0;
+            for(int i = 0; i < this.coefs.Length; i++)
+            {
+                hashCode += (int)(this.coefs[i] * Math.Pow(this.coefs[i], this.degree));
+            }
+            return hashCode;
+        }
+
+        public static Polinomial operator +(Polinomial p1, Polinomial p2)
+        {
+            List<double> newCoef = new List<double>();
+            int maxDegree = (p1.degree > p2.degree) ? p1.degree : p2.degree;
+
+            
+            int lenP1 = p1.coefs.Length;
+            int lenP2 = p2.coefs.Length;
+
+            for (int i = 0; i < maxDegree; i++)
+            {
+                if(i < lenP1 && i < lenP2)
+                {
+                    newCoef.Add(p1.coefs[i] + p2.coefs[i]);
+                }
+
+                if(i >= lenP1)
+                {
+                    newCoef.Add(0 + p2.coefs[i]);
+                }
+
+                if (i >= lenP2)
+                {
+                    newCoef.Add(p1.coefs[i] + 0);
+                }
+            }
+
+            Polinomial newPol = new Polinomial(newCoef.ToArray(), maxDegree);
+
+            return newPol;
+        }
+
+        public static Polinomial operator -(Polinomial p1, Polinomial p2)
+        {
+            List<double> newCoef = new List<double>();
+            int maxDegree = (p1.degree > p2.degree) ? p1.degree : p2.degree;
+
+
+            int lenP1 = p1.coefs.Length;
+            int lenP2 = p2.coefs.Length;
+
+            for (int i = 0; i < maxDegree; i++)
+            {
+                if (i < lenP1 && i < lenP2)
+                {
+                    newCoef.Add(p1.coefs[i] - p2.coefs[i]);
+                }
+
+                if (i >= lenP1)
+                {
+                    newCoef.Add(0 - p2.coefs[i]);
+                }
+
+                if (i >= lenP2)
+                {
+                    newCoef.Add(p1.coefs[i] - 0);
+                }
+            }
+
+            Polinomial newPol = new Polinomial(newCoef.ToArray(), maxDegree);
+
+            return newPol;
+        }
+
+        public static Polinomial operator *(Polinomial p1, Polinomial p2)
+        {
+            int maxDegree = p1.getDegree() + p2.getDegree();
+            double[] coefP3 = new double[maxDegree + 1];
+
+            for (int i = p1.getDegree(); i >= 0; i--)
+            {
+                for (int j = p2.getDegree(); j >= 0; j--)
+                {
+                    coefP3[i + j] += p1.getCoef()[i] * p2.getCoef()[j];
+                }
+            }
+
+            Polinomial p3 = new Polinomial(coefP3, maxDegree);
+            
+            return p3;
+        }
+
+        public static bool operator !=(Polinomial p1, Polinomial p2)
+        {
+            if (p1.getDegree() == p2.getDegree())
+            {
+                for (int i = 0; i < p1.getCoef().Length; i++)
+                {
+                    if (p1.getCoef()[i] != p2.getCoef()[i])
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public static bool operator ==(Polinomial p1, Polinomial p2)
+        {
+            if (p1.getDegree() == p2.getDegree())
+            {
+                for (int i = 0; i < p1.getCoef().Length; i++)
+                {
+                    if (p1.getCoef()[i] != p2.getCoef()[i])
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder polinom = new StringBuilder();
+            for (int i = 0; i <= this.getDegree(); i++)
+            {
+                StringBuilder onePol = new StringBuilder();
+                if(this.getDegree() - i == 0)
+                {
+                    polinom.Append(this.getCoef()[i]);
+                }
+                else
+                {
+                    if(this.getCoef()[i] == 0)
+                    {
+                        polinom.Append("");
+                    }
+                    else
+                    {
+                        if (this.getDegree() - i == 1)
+                        {
+                            if (this.getCoef()[i] == 1)
+                            {
+                                polinom.Append("X + ");
+                            }
+                            else
+                            {
+                                polinom.Append(this.getCoef()[i] + "X + ");
+                            }
+                        }
+                        else
+                        {
+                            polinom.Append(this.getCoef()[i] + "X^" + (this.getDegree() - i) + " + ");
+                        }
+                    }
+                }                
+            }
+            return polinom.ToString();
+        }
+
+        private void IsValid(double[] coefs, int degree = 1)
+        {
+            if(coefs == null)
+            {
+                throw new ArgumentNullException(nameof(coefs));
+            }
+
+            if(degree < 0)
+            {
+                throw new ArgumentException(nameof(degree));
+            }
+        }
+    }
+}
---
+++ NET.S.2018.YACHNIK.05-06/Polynomial/PolinomialOperationsTest.cs
@@ -0,0 +1,124 @@
+﻿using PolinomialExtension;
+using NUnit.Framework;
+using System;
+
+namespace Polynomial.Tests
+{
+    [TestFixture]
+    public class PolinomialOperationsTest
+    {
+        [TestCase(new double[] { 1, 2, 3 }, 2, ExpectedResult = true)]
+        [TestCase(new double[] { 1, 4, 8 }, 2, ExpectedResult = false)]
+        [TestCase(new double[] { 1, 2, 3, 4 }, 3, ExpectedResult = false)]
+
+        [Test]
+        public bool PolinomialEqualsTests(double[] coefs, int degree)
+        {
+            Polinomial firstPol = new Polinomial(new double[] { 1, 2, 3 }, 2);
+            Polinomial secondPol = new Polinomial(coefs, degree);
+
+            return firstPol.Equals(secondPol);
+        }
+
+        [TestCase(new double[] { 1, 2, 3 }, 2, ExpectedResult = 36)]
+        [TestCase(new double[] { 1, 4, 8 }, 2, ExpectedResult = 577)]
+        [TestCase(new double[] { 1, 2, 3, 4 }, 3, ExpectedResult = 354)]
+
+        [Test]
+        public int PolinomialHashCodeTests(double[] coefs, int degree)
+        {
+            Polinomial Pol = new Polinomial(coefs, degree);
+
+            return Pol.GetHashCode();
+        }
+
+        [TestCase(new double[] { 1, 2, 3.5 }, 2, new double[] {2, 4, 6.5}, 2)]
+        [TestCase(new double[] { 1, 2, 3, 4.8 }, 3, new double[] { 2, 4, 6, 4.8 }, 3)]
+
+        [Test]
+        public void PolinomialSumTests(double[] coefs, int degree, double[] sumCoef, int sumDegree)
+        {
+            
+            Polinomial firstPol = new Polinomial(new double[] { 1, 2, 3 }, 2);
+            Polinomial secondPol = new Polinomial(coefs, degree);
+
+            Polinomial sumPol = firstPol + secondPol;
+            Assert.AreEqual(sumPol.getDegree(), sumDegree);
+
+            for (int i = 0; i < sumPol.getCoef().Length; i++)
+            {
+                Assert.AreEqual(sumPol.getCoef()[i], sumCoef[i], 0.1);
+            }
+        }
+
+        [TestCase(new double[] { 1, 7, 3.5 }, 2, new double[] { 0, 5, 0.5 }, 2)]
+        [TestCase(new double[] { 1, 1.8, -10, 4.8 }, 3, new double[] { 0, -0.2, -13, 4.8 }, 3)]
+
+        [Test]
+        public void PolinomialSubtractionTests(double[] coefs, int degree, double[] sumCoef, int sumDegree)
+        {
+
+            Polinomial firstPol = new Polinomial(new double[] { 1, 2, 3 }, 2);
+            Polinomial secondPol = new Polinomial(coefs, degree);
+
+            Polinomial sumPol = secondPol - firstPol;
+            Assert.AreEqual(sumPol.getDegree(), sumDegree);
+
+            for (int i = 0; i < sumPol.getCoef().Length; i++)
+            {
+                Assert.AreEqual(sumPol.getCoef()[i], sumCoef[i], 0.1);
+            }
+        }
+
+        [TestCase(new double[] { 1, 7, 4 }, 2, new double[] { 1, 9, 21, 29, 12 }, 4)]
+        [TestCase(new double[] { 1, 7, 3, 5, 8 }, 4, new double[] { 1, 9, 20, 32, 27, 31, 24}, 6)]
+
+        [Test]
+        public void PolinomialMultiplicationTests(double[] coefs, int degree, double[] sumCoef, int sumDegree)
+        {
+
+            Polinomial firstPol = new Polinomial(new double[] { 1, 2, 3 }, 2);
+            Polinomial secondPol = new Polinomial(coefs, degree);
+
+            Polinomial sumPol = firstPol * secondPol;
+            Assert.AreEqual(sumPol.getDegree(), sumDegree);
+
+            for (int i = 0; i < sumPol.getCoef().Length; i++)
+            {
+                Assert.AreEqual(sumPol.getCoef()[i], sumCoef[i], 0.1);
+            }
+        }
+
+        [TestCase(new double[] { 1, 7, 4 }, 2, new double[] { 1, 7, 4 }, 2, ExpectedResult = true)]
+        [TestCase(new double[] { 1, 7, 3, 5, 9 }, 4, new double[] { 1, 7, 3, 5, 8 }, 4, ExpectedResult = false)]
+
+        [Test]
+        public bool PolinomialEquityTests(double[] coefs1, int degree1, double[] coefs2, int degree2)
+        {
+            Polinomial firstPol = new Polinomial(coefs1, degree1);
+            Polinomial secondPol = new Polinomial(coefs2, degree2);
+            return firstPol == secondPol;
+        }
+
+        [TestCase(new double[] { 1, 7, 4, 5 }, 3, new double[] { 1, 7, 4 }, 2, ExpectedResult = true)]
+        [TestCase(new double[] { 1, 7, 3, 5, 8 }, 4, new double[] { 1, 7, 3, 5, 8 }, 4, ExpectedResult = false)]
+
+        [Test]
+        public bool PolinomialUnequityTests(double[] coefs1, int degree1, double[] coefs2, int degree2)
+        {
+            Polinomial firstPol = new Polinomial(coefs1, degree1);
+            Polinomial secondPol = new Polinomial(coefs2, degree2);
+            return firstPol != secondPol;
+        }
+
+        [TestCase(new double[] { 1, 7, 4, 1 }, 3, ExpectedResult = "1X^3 + 7X^2 + 4X + 1")]
+
+        [Test]
+        public string PolinomialToStringTests(double[] coefs1, int degree1)
+        {
+            Polinomial firstPol = new Polinomial(coefs1, degree1);
+            return firstPol.ToString();
+        }
+
+    }
+}
