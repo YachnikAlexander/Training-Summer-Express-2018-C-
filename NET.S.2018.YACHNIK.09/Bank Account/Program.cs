e9757268b273d9a927c19e706cacac0c03da78dc
Yachnik Alexander <sansanich230899@gmail.com>
2018-07-15 17:04:45 +0300

    added bankAccount

---
+++ NET.S.2018.YACHNIK.09/Bank Account/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankAccaunt;
+
+namespace BankAccout
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc = new Account("Sasha", "Yachnik", 100000000);
+        }
+    }
+}
