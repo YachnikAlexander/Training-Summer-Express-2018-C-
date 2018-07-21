43f316c338988acc517f7a1ddff1a02375ccab90
Yachnik Alexander <sansanich230899@gmail.com>
2018-07-21 20:19:39 +0300

    Change console messageManager

--- NET.S.2018.YACHNIK.14/MessageMeneger/Console/Program.cs
+++ NET.S.2018.YACHNIK.14/MessageMeneger/Console/Program.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Manager;
+using Observers;
 
-namespace MessageManager
+namespace SendingMessages
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var manager = new MessageManager(10);
+            var user = new User(manager);
+            var admin = new Administrator(manager);
+            Console.WriteLine();
+            manager.SimulateNewMessage("Minsk", "Riga", "Letter");
+
+            user.StopMessage();
+            Console.WriteLine();
+            manager.SimulateNewMessage("Warsawa", "Minsk", "SMS");
+            Console.ReadKey();
         }
     }
 }
